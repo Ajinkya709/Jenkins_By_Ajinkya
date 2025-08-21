e9d3fff669241b038a2b080ff5f12198492f0203
D6_Ajinkya_63032 <ajinkyagurav709@gmail.com>
2025-08-21 11:56:49 +0530

    upated code

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Jenkins! This is a helllo simple Python app.by aj"
+    return "Hello, ainexus"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
